f5ae50f83f841e2d6f1475886fbc0605ecc88684
Eugen Kiss <eugen@eugenkiss.com>
2018-01-28 17:42:13 +0100

    WIP: Improvements

--- src/app/comps/about.tsx
+++ src/app/comps/about.tsx
@@ -2,19 +2,23 @@ import * as React from 'react'
 import {Component} from 'react'
 import {inject, observer} from 'mobx-react'
 import {css} from 'emotion'
-import {A, Flex} from './basic'
+import {A, Flex, Span} from './basic'
 import {Store} from '../store'
+import {DATE} from '../cfg'
 
 @inject('store') @observer
 export class About extends Component<{store?: Store}> {
   render() {
     return (
       <Flex
+        f={2}
+        flexDirection='column'
         justify='center'
         align='center'
         className={css`
         height: 100%;
       `}>
+        <Span mb={2}>Version: {DATE.toUTCString()}</Span>
         <A href='https://github.com/eugenkiss/hnclient'>
           https://github.com/eugenkiss/hnclient
         </A>
